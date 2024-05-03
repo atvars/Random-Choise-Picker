b54609290728fdf99b0e62575b78a48b3cdabf35
atvars <avelavs@gmail.com>
2024-05-03 22:35:14 +0000

    added rest of the logic to my random picker

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ textarea.focus();
 // event listener which listns to every key release
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+        randomSelect()
+    }
 })
 
 // seperats entered strings when comma is entered and saves in array. Also removes any white spaces
@@ -24,4 +31,45 @@ function createTags(input) {
         tagEl.innerText = tag
         tagsEl.appendChild(tagEl)
     })
+}
+
+// picks a rondom tag by chamiping with highlight class from one to another
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+    }, 100)
+
+    // makes sure it stops on one of them
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+            
+        }, 100)
+
+    }, times * 100)
+}
+
+// function to pick a random
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+//  add/remove highlight class
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
